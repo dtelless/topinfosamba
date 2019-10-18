a6cc8fd306a6aea31cd5cf4492e9a64baef29a57
Daniel Telles <dtelless@gmail.com>
2019-10-18 20:03:55 +0000

    Teste

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 apt-cache policy docker-ce
 echo "{\"iptables\": false}" > /etc/docker/daemon.json
 /etc/init.d/docker restart
